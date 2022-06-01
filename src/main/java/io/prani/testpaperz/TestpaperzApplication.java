d9712390e9ec382704d030a61a542faa2dc51954
pranita-s-chavan <pranita.s.chavan@outlook.com>
2022-06-01 19:34:02 -0400

    sample backend

---
+++ src/main/java/io/prani/testpaperz/TestpaperzApplication.java
@@ -0,0 +1,13 @@
+package io.prani.testpaperz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpaperzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpaperzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/prani/testpaperz/TestpaperzApplicationTests.java
@@ -0,0 +1,13 @@
+package io.prani.testpaperz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpaperzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
